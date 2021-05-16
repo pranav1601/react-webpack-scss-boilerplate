07cdd121eac32401a68bd5c9e8fbbabedcf14724
pranav1601 <pranav.agarw@gmail.com>
2021-05-16 08:56:57 +0530

    created boilerplate

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Component} from './components/Component.js'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+ReactDOM.render(<Component/>,document.getElementById('app'))
---
+++ src/components/Component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Component extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>This is a component</p>
+            </div>
+        )
+    }
+}
+
+export {Component}
